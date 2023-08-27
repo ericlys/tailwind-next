9e0eeed0f42a6462035855c02883d52275b3940a
Ericlys Moreira <ericlysm@gmail.com>
2023-08-26 23:25:14 -0300

    chore: eslint and prettier setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,12 @@
 import './globals.css'
+
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '700', '800'],
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
