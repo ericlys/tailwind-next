697b2f1a6e4f9d95cc22c8bd38d2b65db9dcf636
Ericlys Moreira <ericlysm@gmail.com>
2023-08-27 16:08:20 -0300

    chore: used space widget

--- src/app/components/Sidebar/NavItem.tsx
+++ src/app/components/Sidebar/NavItem.tsx
@@ -12,7 +12,7 @@ export function NavItem({ title, icon: Icon }: NavItemProps) {
       href=""
       className="group flex items-center gap-3 rounded px-3 py-2 hover:bg-violet-50"
     >
-      <Icon className="h-5 w-5 text-zinc-500 " />
+      <Icon className="h-5 w-5 text-zinc-500 group-hover:text-violet-500" />
       <span className="font-medium text-zinc-700 group-hover:text-violet-500">
         {title}
       </span>
