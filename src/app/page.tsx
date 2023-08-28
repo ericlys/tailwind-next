701310a52111169899cf25fe86d0b1ed42677b6a
Ericlys Moreira <ericlysm@gmail.com>
2023-08-28 10:36:41 -0300

    chore: componentizing the select

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import * as Input from './components/Input'
 import { Mail } from 'lucide-react'
 import * as FileInput from './components/Form/FileInput'
 import { Select } from './components/Form/Select'
+import { SelectItem } from './components/Form/Select/SelectItem'
 
 export default function Home() {
   return (
@@ -109,7 +110,10 @@ export default function Home() {
             >
               Country
             </label>
-            <Select />
+            <Select placeholder="Select a country...">
+              <SelectItem value="br" text="Brazil" />
+              <SelectItem value="us" text="United States" />
+            </Select>
           </div>
 
           <div className="grid grid-cols-form gap-3 pt-5">
@@ -119,7 +123,13 @@ export default function Home() {
             >
               TimeZone
             </label>
-            <div />
+            <Select placeholder="Select a timezone...">
+              <SelectItem
+                value="utc8"
+                text="Pacific Standard Time (UTC−08:00)"
+              />
+              <SelectItem value="utc3" text="America São Paulo (UTC-03:00)" />
+            </Select>
           </div>
 
           <div className="grid grid-cols-form gap-3 pt-5">
